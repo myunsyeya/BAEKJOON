ec3711bf3f2ba45c9f860d8cef036f5c31c58312
myunsyeya <ggie1212@naver.com>
2023-08-17 01:43:58 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 276 ms, Memory: 4068 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,9 +1,15 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int arr[1048576];
+/*
+* counting sort 사용.
+* 절대값 1,000,000 이하이므로 1048576를 편향치(bias)로 잡고 배열 크기는 2097152로 사용.
+* 아차차.. stack 내부는 보통 1mb 이하겠지..? 
+*/
+const int kInputArea = 2097152;
+const int kBias = kInputArea / 2;
+bool arr[kInputArea];
 
 int main(void)
 {
@@ -11,16 +17,15 @@ int main(void)
     cout.tie(NULL); cin.tie(NULL);
 
     int n, temp;
-
+    
     cin >> n;
-
     for (int i = 0; i < n; i++)
     {
         cin >> temp;
-        arr[i] = temp;
+        arr[temp + kBias] = true;
     }
-    sort(arr, arr + n);
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << '\n';
+    for (int i = 0; i < kInputArea; i++)
+        if (arr[i]) cout << i - kBias << '\n';
+
     return 0;
 }
