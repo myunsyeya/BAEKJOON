9350c300636fff359a25769df0ee2ca1d2b91435
myunsyeya <ggie1212@naver.com>
2023-08-22 10:47:44 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 4264 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -4,9 +4,12 @@
 
 using namespace std;
 /*
+* !핵심은 두 가지!
+* !1부터 N까지 모든 수는 중복 없는 정수!, 따라서 오름차순으로 넣으면 1부터 N까지 삽입!
+* !stack의 top이 수열의 값보다 크면, 원하는 수열을 만들 수 없으므로(여러 번 pop해야 하므로) 불가!
 * series_i = answer_i = 0 인덱스를 따로 설정.
 * for(N만큼 반복) {
-*	now(현재 sorted_series 데이터 값)
+*	now(현재 데이터 값)
 *	now를 stack에 push
 *	series와 stack.top이 같을 때 동안
 *	sries_i 증가
@@ -23,7 +26,7 @@ int main(void)
 	cout.tie(NULL); cin.tie(NULL);
 	
 	int n, temp, series_i;
-	vector<int> stack, series, sorted_series, answer;
+	vector<int> stack, series, answer;
 
 	cin >> n;
 	for (int i = 0; i < n; i++)
@@ -31,13 +34,11 @@ int main(void)
 		cin >> temp;
 		series.push_back(temp);
 	}
-	sorted_series = series;
-	sort(sorted_series.begin(), sorted_series.end());
 	
 	series_i = 0;
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		stack.push_back(sorted_series[i]);
+		stack.push_back(i);
 		answer.push_back(1);
 		while (!stack.empty() && stack.back() == series[series_i])
 		{
@@ -45,6 +46,8 @@ int main(void)
 			stack.pop_back();
 			answer.push_back(0);
 		}
+		if (!stack.empty() && stack.back() > series[series_i])
+			break;
 	}
 	if (stack.empty())
 		for (int i = 0; i < answer.size(); i++)
