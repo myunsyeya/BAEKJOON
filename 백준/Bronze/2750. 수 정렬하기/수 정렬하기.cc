7a7197161a9c39997aa1d2c1c9544286630bc496
myunsyeya <ggie1212@naver.com>
2023-08-25 10:49:42 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,25 +1,35 @@
 #include <iostream>
-#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
-
 int main(void)
 {
-    ios::sync_with_stdio(false);
-    cout.tie(NULL); cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cout.tie(NULL); cin.tie(NULL);
+	
+	int n;
+	int* arr;
+	cin >> n;
+	arr = new int[n];
+	copy_n(istream_iterator<int>(cin), n, arr);
 
-    int n, temp;
-    int arr[1024] = {0,};
-    cin >> n;
-    
-    for(int i = 0; i < n; i++)
-    {
-        cin >> temp;
-        arr[i] = temp;
-    }
-    sort(arr, arr + n);
-    for(int i = 0; i < n; i++)
-        cout << arr[i] << endl;
-    return 0;
+	// 향상된 버블정렬
+	for(int cnt = 0, i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				cnt++;
+			}
+		}
+		if (!cnt) break;
+	}
+
+	copy(arr, arr + n, ostream_iterator<int>(cout, "\n"));
+	return 0;
 }
