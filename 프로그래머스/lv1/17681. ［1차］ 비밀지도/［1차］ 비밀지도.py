d9d7ea076273a9726d42d34b9ee9aa8f77a3bccf
myunsyeya <ggie1212@naver.com>
2023-09-28 16:21:05 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,3 +1,3 @@
 def solution(n, arr1, arr2):
-    answer = [bin(elm1|elm2)[2:].zfill(n).replace('1','#').replace('0',' ') for elm1, elm2 in zip(arr1, arr2)]
+    answer = [bin(i|j)[2:].zfill(n).replace('1','#').replace('0',' ') for i,j in zip(arr1,arr2)]
     return answer
