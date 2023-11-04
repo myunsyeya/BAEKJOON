f3e000695c6ddb2b6488710c14137c869c60e01a
myunsyeya <ggie1212@naver.com>
2023-11-04 21:42:36 +0900

    [level 2] Title: 프로세스, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,19 @@
+from collections import deque
+def solution(priorities, location):
+    answer = 0
+    deq = deque()
+    deq.extend(priorities)
+    
+    while deq:
+        now = deq.popleft()
+        if deq and now < max(list(deq)):
+            deq.append(now)
+            if not location:
+                location = len(deq)
+        else:
+            answer += 1
+            if not location:
+                break
+        location -= 1
+                
+    return answer
