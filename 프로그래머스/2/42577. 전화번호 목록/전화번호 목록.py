fb68f41dc0b5d4da21454aefd224799160f81d41
myunsyeya <ggie1212@naver.com>
2023-11-18 22:16:33 +0900

    [level 2] Title: 전화번호 목록, Time: 514.28 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,9 @@
 def solution(phone_book):
     answer = True
     dic = {i: 1 for i in phone_book}
+    # O(20*n) = O(n)
     for phone in phone_book:
         for i in range(len(phone)):
-            if phone[:i] in dic and phone[:i] != phone:
+            if phone[:i] in dic:
                 answer = False
     return answer
