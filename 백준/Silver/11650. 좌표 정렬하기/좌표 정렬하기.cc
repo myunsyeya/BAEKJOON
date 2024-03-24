a256e875e90a41d0e044f216b86e7b75c572ef1f
myunsyeya <ggie1212@naver.com>
2024-03-24 17:24:21 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 48 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<pair<int,int>> v;
+
+ostream& operator<<(ostream& o, const pair<int,int>& p)
+{
+    return o << p.first << " " << p.second;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int N;
+    int x, y;
+    cin >> N;
+    while (N--) {
+        cin >> x >> y;
+        v.push_back({ x, y });
+    }
+    sort(v.begin(), v.end(), [](pair<int, int> a, pair<int, int> b) {
+        if (a.first == b.first) return a.second < b.second;
+        return a.first < b.first;
+        });
+    for_each(v.begin(), v.end(), [](const pair<int, int>& p) {
+        cout << p << "\n";
+        });
+    return 0;
+}
