76d6dc3e065c140165c5da8bf4e514137de86598
myunsyeya <ggie1212@naver.com>
2024-05-16 20:21:50 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,47 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <algorithm>
 
-int compare(int* a, int* b) {
-    return (*a == *b) ? 0 : (*a > * b) ? 1 : -1;
-}
+using namespace std;
 
-int dfs(int (*_a)[27], int (*_v)[27], int _y, int _x) {
-    int deltaX[4] = { 1, 0, -1, 0 };
-    int deltaY[4] = { 0, 1, 0, -1 };
-    int cnt = 1;
+#define fio() cin.tie(0), ios_base::sync_with_stdio(0)
 
-    _v[_y][_x] = 1;
+vector<int> answer;
+int dr[4] = { 0, 0, 1, -1 };
+int dc[4] = { 1, -1, 0, 0 };
+int map[30][30];
+int visited[30][30];
 
-    for (int i = 0; i < 4; i++) {
-        int nx = _x + deltaX[i];
-        int ny = _y + deltaY[i];
-        if (_a[ny][nx] && !_v[ny][nx])
-            cnt += dfs(_a, _v, ny, nx);
+int main()
+{
+    fio();
+    int n; cin >> n;
+    for (int i = 1; i <= n; i++) {
+        string bits; cin >> bits;
+        for (int j = 1; j <= n; j++) map[i][j] = bits[j-1] - '0'; 
     }
 
-    return cnt;
-}
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (!map[i][j] || visited[i][j]) continue;
+            /* bfs */
+            int cnt = 1;
+            queue<tuple<int,int>> q;
+            q.push(make_tuple(i, j));
+            visited[i][j] = 1;
 
-int main() {
-    int count = 0;
-    int HN[128] = { 0, };
-    int a[27][27] = { 0, };
-    int v[27][27] = { 0, };
-    int N = 0;
-
-    scanf("%d", &N);
-
-    for(int i = 1; i <= N; i++)
-        for (int j = 1; j <= N; j++)
-            scanf("%1d", &a[i][j]);
-
-    for (int i = 1; i <= N; i++)
-        for (int j = 1; j <= N; j++)
-            if (a[i][j] && !v[i][j])
-                HN[count++] = dfs(a, v, i, j);
-
-    qsort(HN, count, sizeof(int), (int(*)(const void*, const void*))compare);
-
-    printf("%d\n", count);
-    for (int i = 0; i < count; i++) printf("%d\n", HN[i]);
+            while (!q.empty()) {
+                int cr, cc;
+                tie(cr, cc) = q.front(); q.pop();
+                
+                for (int i = 0; i < 4; i++) {
+                    int nr = cr + dr[i];
+                    int nc = cc + dc[i];
+                    if (!map[nr][nc] || visited[nr][nc]) continue;
+                    visited[nr][nc] = 1;
+                    q.push({nr, nc});
+                    cnt++;
+                }
+            }
+            answer.push_back(cnt);
+        }
+    }
+    sort(answer.begin(), answer.end());
+    cout << answer.size() << '\n';
+    for (int i : answer) cout << i << '\n';
+    return 0;
 }
