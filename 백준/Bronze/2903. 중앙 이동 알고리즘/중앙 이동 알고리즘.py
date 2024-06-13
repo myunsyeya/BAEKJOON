e93ba37c4d667478e86848416a4584d55b79a091
myunsyeya <ggie1212@naver.com>
2024-06-13 19:44:59 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
@@ -1,8 +1,11 @@
-def recur(p):
-    return p + p-1
+#def recur(p):
+#    return p + p-1
+# a_(n+1) = 2*a_n - 1
+# a_(n+1)-1 = 2*(a_n-1)
+# b_(n+1) = 2b_n
+# b_n = b_1*2^(n-1)
+# a_n-1 = (3-1)*2^(n-1), because a_1 = 3
+# a_n = 2^n+1
 
 N = int(input())
-point = 2
-for _ in range(N): 
-    point = recur(point)
-print(point**2)
+print((2**N+1)**2)
