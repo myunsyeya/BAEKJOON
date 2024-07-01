2e9a139d057d5489bd4bf85ab80656a4a5855443
myunsyeya <ggie1212@naver.com>
2024-07-01 18:22:17 +0900

    [Silver II] Title: 나무 자르기, Time: 148 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -15,14 +15,14 @@ int main()
 
     int le = 0;
     int ri = (int)1e9;
-    int h = 0;
+    int h;
 
     while (le <= ri) {
-        int mid = (le + ri) >> 1;
+        int mid = ri - (ri-le)/2;
         long long sum = 0;
         for (const int& w : wood) sum += (w-mid > 0) ? w-mid : 0;
         if (sum >= M) {
-            h = max(h, mid);
+            h = mid;
             le = mid + 1;
         } else {
             ri = mid - 1;
