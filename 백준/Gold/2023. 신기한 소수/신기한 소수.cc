a5cf9eb8849fbde8e873cf32702a06cfab01f976
myunsyeya <ggie1212@naver.com>
2024-07-03 02:36:25 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.cc
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -1,61 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-#define fio() do { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); } while(0)
-#define MAX ( 32000 ) // sqrt(10^9) = 31622.7...
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
 
-bool not_prime[MAX];
-vector<int> prime;
-vector<int> intp;
+typedef long long ll;
 
-void sieve()
-{
-    not_prime[1] = 1;
-    for (int i = 2; i < MAX; i++) {
-        if (not_prime[i]) continue;
-        prime.push_back(i);
-        for (int j = i * i; j < MAX; j += i) not_prime[j] = 1;
-    }
-}
-
-bool isPrime(int n)
+bool isPrime(int n) 
 {
     if (n == 1) return false;
-    for (int x : prime) {
-        if (x * x > n) break;
-        if (n == x) return true;
-        if (n % x == 0) return false;
+    for (int i = 2; i*i <= n; i++) {
+        if (n % i == 0) return false;
     }
     return true;
 }
 
-int power(int a, int b)
+ll pow(int n, int r) 
 {
-    if (!b) return 1;
-    if (b % 2) return a * power(a, b - 1);
-    int half = power(a, b / 2);
-    return half * half;
+    if (!r) return 1;
+    return n*pow(n, r-1);
 }
 
 int main()
 {
     fio();
-    int n, max;
-    cin >> n;
-    max = power(10, n);
-    sieve();
-    for (int i = power(10, n - 1); i < max; i++) {
-        int check = true;
-        for (int j = power(10, n - 1); j > 0; j/=10)
-            if (!isPrime(i / j))
-            {
-                check = false;
-                break;
-            }
-        if (check) intp.push_back(i);
+    int N; cin >> N;
+
+    int p = pow(10, N-1);
+    int i = p;
+    while (i < p*10) {
+        for (int j = N; j > 0; j--) {
+            if (!isPrime(i/pow(10, j-1))) { i += pow(10, j-1); goto fail; }
+        }
+        cout << i << '\n';
+        i++;
+fail:;
     }
-    for (int x : intp) cout << x << '\n';
     return 0;
 }
