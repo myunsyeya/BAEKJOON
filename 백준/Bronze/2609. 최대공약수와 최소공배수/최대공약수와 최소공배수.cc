5b06c086520834f5ab212d31198f5c5ffae4ca32
myunsyeya <ggie1212@naver.com>
2024-07-03 00:14:07 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-#define fio() do { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); } while(0)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
 
-int __gcd(int a, int b) {
+int gcd(int a, int b) 
+{
     if (!b) return a;
-    return __gcd(b, a%b);
+    return gcd(b, a%b);
 }
 
 int main()
 {
-    int a, b, gcd, lcm;
-    cin >> a >> b;
+    fio();
+    int A, B; cin >> A >> B;
+    cout << gcd(A, B) << '\n' << A*B/gcd(A,B);
 
-    gcd = (a<b) ? __gcd(b,a) : __gcd(a,b);
-    lcm = a*b/gcd;
-    cout << gcd << '\n' << lcm;
     return 0;
 }
