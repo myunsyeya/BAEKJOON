779f46dcb01870ad997b945154a0a97e12dd15fe
myunsyeya <ggie1212@naver.com>
2024-07-04 08:19:56 +0900

    [Gold V] Title: 리모컨, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.cc
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -1,45 +1,37 @@
 #include <iostream>
-#include <cstdio>
-#include <cstring>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-#define fio() do { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); } while(0)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
+#define ABS(x) ( ((x) > 0) ? (x) : (-(x)) )
 
-#define MAX 1000000
-
-bool button[10], cantry[MAX];
+bool button[10];
 
 int main()
 {
-    int N, M;
-	int min = MAX;
-    int idx = MAX;
-    char str_num[10];
-	cin >> N >> M;
-    while (M--) {
-        int x;
-        cin >> x;
-        button[x] = true;
-    }
-    for (int i = 0; i < MAX; i++) {
-        sprintf(str_num, "%d", i);
-        for (int j = 0; j < strlen(str_num); j++)
-            if (button[str_num[j] - '0']) { cantry[i] = true; break; }
+    fio();
+    int N, M; cin >> N >> M;
+    for (int i = 0; i < M; i++) {
+        int b; cin >> b;
+        button[b] = true;
     }
 
-    for (int i = 0; i < MAX; i++) {
-        if (!cantry[i]) 
-        {
-            if (N > i) { 
-                if (N - i < min) { min = N - i; idx = i; } 
-            } else if (i - N < min) { min = i - N; idx = i; }
+    int direct = (button[0]) ? -100 : 0;
+    int radix = 0;
+
+    for (int i = 1; i <= (int)1e6; i++) {
+        for (int j = i; j; j/=10) {
+            if (button[j%10]) goto fail;
         }
+        if (ABS(N-i) < ABS(N-direct)) direct = i;
+fail:;
     }
-    sprintf(str_num, "%d", idx);
-    if ((100 - N >= 0) && (100 - N < min + strlen(str_num))) cout << 100 - N;
-    else if ((N - 100 > 0) && (N - 100 < min + strlen(str_num))) cout << N - 100;
-    else cout << min + strlen(str_num);
-	return 0;
+    
+    if (!direct) radix = 1;
+    else for (int i = direct; i; i/=10) radix++;
+
+    cout << ((ABS(N-100) < ABS(N-direct)+radix) ? ABS(N-100) : ABS(N-direct)+radix);
+    return 0;
 }
