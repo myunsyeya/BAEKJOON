3f3d3f36d7fd9c9c43e16bfed05761f81b8c8d67
myunsyeya <ggie1212@naver.com>
2024-07-16 17:02:46 +0900

    [Silver IV] Title: 다음 소수, Time: 2332 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.py
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -1,7 +1,10 @@
 T = int(input())
 for _ in range(T):
     N = int(input())
+    if N == 0 or N == 1: N = 2
     while True:
+        if not N % 2 and N != 2: N += 1
+        elif not N % 3 and N != 3: N += 2
         for i in range(2, int(N**0.5+1)):
             if N%i == 0: break
         else: break
