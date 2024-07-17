8b4849b4f7a24715d7d16768c29991a9efd160fa
myunsyeya <ggie1212@naver.com>
2024-07-18 08:53:55 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 1636 ms, Memory: 43668 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left
+max_N = 1_000_000+1
+era = [1]*max_N
+era[0]=era[1]=0
+for i in range(int(max_N**0.5)+1):
+    if era[i]:
+        for j in range(i*i, max_N, i): era[j]=0
+prime = [i for i in range(max_N) if era[i]]
+
+T, *N = map(int, [*open(0)])
+for n in N:
+    part = 0
+    for p in prime:
+        if p+p > n: break
+        if prime[bisect_left(prime, n-p)] == n-p: part+=1
+    print(part)
