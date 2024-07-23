e363c6fea992aed6541cd2d596654c646ff3061e
myunsyeya <ggie1212@naver.com>
2024-07-24 08:06:55 +0900

    [Silver IV] Title: 덱 2, Time: 788 ms, Memory: 167760 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -0,0 +1,17 @@
+from collections import deque
+N, *I = map(int, open(0).read().split())
+I = deque(I)
+deq = deque()
+
+instructions = {
+    1: lambda: deq.appendleft(I.popleft()),
+    2: lambda: deq.append(I.popleft()),
+    3: lambda: print(deq.popleft() if deq else -1),
+    4: lambda: print(deq.pop() if deq else -1),
+    5: lambda: print(len(deq)),
+    6: lambda: print(1 if not deq else 0),
+    7: lambda: print(deq[0] if deq else -1),
+    8: lambda: print(deq[-1] if deq else -1)
+}
+
+while I: instructions[I.popleft()]()
