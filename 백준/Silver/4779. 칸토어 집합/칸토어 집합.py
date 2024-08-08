e5808172d3f1a184f2914adb3d07bafd3de9fded
myunsyeya <ggie1212@naver.com>
2024-08-08 09:41:56 +0900

    [Silver III] Title: 칸토어 집합, Time: 56 ms, Memory: 36652 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,10 +1,6 @@
-def go(S, l, r):
-    div = (r-l+1)//3
-    if r>l+div-1: go(S, l, l+div-1)
-    if r-div+1>l: go(S, r-div+1, r)
-    for i in range(div): S[l+div+i] = ' '
+def go(n):
+    if not n: return '-'
+    return go(n-1) + ' '*3**(n-1) + go(n-1)
 
 for N in map(int, [*open(0)]):
-    string = ['-']*3**N
-    go(string, 0, 3**N-1)
-    print(''.join(string))
+    print(''.join(go(N)))
