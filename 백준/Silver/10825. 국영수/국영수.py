c43f2f76a29d5b8162e85f4630532854e188c1c7
myunsyeya <ggie1212@naver.com>
2024-09-19 19:22:56 +0900

    [Silver IV] Title: 국영수, Time: 380 ms, Memory: 95452 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -1,4 +1,4 @@
-N = int(input())
-lis = []
-for _ in range(N): lis.append(input().split())
-for i in sorted(lis, key=lambda x: (-int(x[1]),int(x[2]),-int(x[3]),x[0])): print(i[0])
+N, *lis = open(0)
+li = []
+for l in lis: li.append(l.split())
+for i in sorted(li, key=lambda x: (-int(x[1]),int(x[2]),-int(x[3]),x[0])): print(i[0])
