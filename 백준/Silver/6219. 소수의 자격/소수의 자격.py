93e715568e3f528ab1f6f2dd3b8fd92b3b650a5f
myunsyeya <ggie1212@naver.com>
2024-09-28 01:57:12 +0900

    [Silver III] Title: 소수의 자격, Time: 292 ms, Memory: 72792 KB -BaekjoonHub

--- 백준/Silver/6219. 소수의 자격/소수의 자격.py
+++ 백준/Silver/6219. 소수의 자격/소수의 자격.py
@@ -1,13 +1,13 @@
 A, B, C = map(int, input().split())
 C = str(C)
-toB = [1] * (B + 1)
+seive = [1] * (B + 1)
 answer = 0
 
 if A <= 2 <= B and '2' in C: answer += 1
 
 for i in range(3, B + 1, 2):
-    if toB[i]:
+    if seive[i]:
         if i >= A and C in str(i): answer += 1
-        for j in range(i * i, B + 1, i * 2): toB[j] = 0
+        if i * i <= B: seive[i*i::i*2] = [0]*((B-i*i)//(i*2)+1)
 
 print(answer)
