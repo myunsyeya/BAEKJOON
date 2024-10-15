1a6fc6c15b4b0bda21f813bb8e10d17b35b67f8c
myunsyeya <ggie1212@naver.com>
2024-10-15 15:34:18 +0900

    [Silver III] Title: 두 수의 합, Time: 76 ms, Memory: 45628 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -3,12 +3,8 @@ x = lis[-1]
 lis = lis[:-1]
 answer = 0
 
-le = 0
-ri = len(lis)-1
-lis.sort()
-while le < ri:
-    s = lis[le]+lis[ri]
-    if s < x: le+=1
-    elif s > x: ri-=1
-    else: answer+=1; ri-=1; le+=1
+chk = dict()
+for i in lis:
+    if x-i in chk: answer+=1
+    chk[i] = True
 print(answer)
