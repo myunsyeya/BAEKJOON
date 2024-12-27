46037b2979fcd2e40278834dca0b5cb9899ec08e
myunsyeya <ggie1212@naver.com>
2024-12-28 00:04:42 +0900

    [Silver III] Title: 주유소, Time: 12 ms, Memory: 2804 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define fio() cin.tie(0), ios_base::sync_with_stdio(0)
+
+int main()
+{
+    fio();
+    int N; cin >> N;
+    int answer = 0;
+    int min_price = 1e9;
+    vector<int> length(N-1);
+    vector<int> price(N);
+    for (int &l : length) cin >> l;
+    for (int &p : price) cin >> p;
+
+    for (int i = 0; i < N-1; i++) {
+        min_price = min(min_price, price[i]);
+        answer += min_price * length[i];
+    }
+    cout << answer;
+    return 0;
+}
